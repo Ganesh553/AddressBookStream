af4129cba95b09a3e836cd3d79dc5ebace9650ef
unknown <Ganesh553>
2021-01-05 18:12:53 +0530

    Adding Person TO AddressBook

---
+++ src/main/java/service/AddressBook.java
@@ -0,0 +1,16 @@
+package service;
+
+import model.Person;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddressBook {
+
+    ArrayList<Person> al = new ArrayList<Person>();
+    Scanner sc = new Scanner(System.in);
+
+    public void addingNewPerson(Person obj) {
+        al.add(obj);
+    }
+}
